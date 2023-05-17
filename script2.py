20ee3000ba3b6d5f54d4f89ebbc7cd7738a90cf8
Helin128 <133912785+Helin128@users.noreply.github.com>
2023-05-17 16:21:11 -0400

    Modified both files in the repo clone
    -Deleted a print() statement from script1.py
    -Added two print() statements to script2.py

--- script2.py
+++ script2.py
@@ -1,3 +1,4 @@
 print("Beautiful is better than ugly.")
 print("Flat is better than dense.")
 print("Althorough that way may not be pbvious at first unless you're Dutch.")
+print("Avo kabhi haveli pe")
